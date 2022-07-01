d460dfe151ae0be894a112006e08b3e6b3f493a6
RespawnR <respawnr@qq.com>
2022-07-02 02:30:44 +0800

    refactor: 修改初始化配置文件

--- init.sh
+++ init.sh
@@ -12,7 +12,4 @@ cp -r source source_bak
 
 # 安装、配置主题
 git clone https://github.com/ppoffice/hexo-theme-icarus themes/icarus
-npm install semver --save
-npm install --save bulma-stylus@0.8.0 hexo@^6.0.0 hexo-component-inferno@^1.1.0 
-npm install --save hexo-log@^3.0.0 hexo-pagination@^2.0.0 hexo-renderer-inferno@^0.1.3 
-npm install --save inferno@^7.3.3 inferno-create-element@^7.3.3
+npm install --save semver bulma-stylus@0.8.0 hexo@^6.0.0 hexo-component-inferno@^1.1.0 hexo-log@^3.0.0 hexo-pagination@^2.0.0 hexo-renderer-inferno@^0.1.3 inferno@^7.3.3 inferno-create-element@^7.3.3
